e841166515f9f89b44e40b06ec9bdb36cb96ead9
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-17 17:30:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,7 @@ end
 def play(songs_ary)
   puts "Please enter a song name or number:"
   response = gets.strip()
-  if response.to_i() > 0 and response.to_i() < song
+  if response.to_i() > 0 and response.to_i() < songs_ary.count + 1 
 
     end
   end
