4a34a9003920268fda1d596b764332be9c3961d7
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-17 17:22:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -4,6 +4,18 @@ LIST = "list" # list --> list the songs the user can play
 PLAY = "play" # play --> should ask the user to input the name of a song OR a track number
 EXIT = "exit" # exit --> should say goodbye, and end the program
 
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 def help
 end
 
@@ -18,4 +30,4 @@ def exit_jukebox
 end
 
 def run
-end 
+end
