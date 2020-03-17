eba9fc43cd4ed42cc28a7b0e80522871159aa5e7
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-17 18:26:17 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,7 +33,7 @@ def play(songs_ary)
   else
     i = response.to_i()
     if i < songs_ary.count and i > 0
-      puts "Playing #{songs_ary.at(i)}"
+      puts "Playing #{songs_ary.at(i + 1)}"
     else
       puts "Invalid input, please try again"
     end
