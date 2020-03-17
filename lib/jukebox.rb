7ba2e17f2666c980847cb3193c67787c52686907
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-17 17:20:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,4 @@
-# Commands 
-HELP = "help" #  help --> ouput instructions for the user on how to use the jukebox
-LIST = "list"
-PLAY = "play"
+# Commands
+HELP = "help" # help --> ouput instructions for the user on how to use the jukebox
+LIST = "list" # list --> list the songs the user can play
+PLAY = "play" # play --> should ask the user to input the name of a song OR a track number 
