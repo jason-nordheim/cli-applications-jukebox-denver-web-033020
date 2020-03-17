85a63d4289228075b563a175184057d9933f5b15
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-17 17:21:43 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -3,3 +3,17 @@ HELP = "help" # help --> ouput instructions for the user on how to use the jukeb
 LIST = "list" # list --> list the songs the user can play
 PLAY = "play" # play --> should ask the user to input the name of a song OR a track number
 EXIT = "exit" # exit --> should say goodbye, and end the program
+
+def help
+  
+end
+
+def play 
+end 
+
+def list
+  
+end
+
+def exit_jukebox 
+end 
